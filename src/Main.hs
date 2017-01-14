3d7f6fffe767f92b7359c618ac2164acec7ed75f
uwap <me@uwap.name>
2017-01-14 15:06:13 +0100

    Add cli option for path to config

--- src/Main.hs
+++ src/Main.hs
@@ -8,23 +8,25 @@ import Lucid
 import Network.HTTP.Types.Status
 import Network.Wai.Handler.Warp
 import Network.Wai
-import Data.String (fromString)
+import Data.String (String, fromString)
 import Options.Applicative hiding (auto)
 
 import Types
 import Pages
 import Database
 
-loadConfig :: Bool -> IO Config
-loadConfig b = input auto "./config" & if b then detailed else identity
+loadConfig :: Bool -> LText -> IO Config
+loadConfig b path = input auto path & if b then detailed else identity
 
 getSettings :: Config -> Settings
 getSettings Config {..} = defaultSettings
                         & setPort (fromIntegral port)
                         & setHost (fromString $ toS host)
 
-cli :: Parser Bool
-cli = switch $ long "verbose" <> short 'v' <> help "Show detailed error messages"
+cli :: Parser (Bool, String)
+cli = (,)
+  <$> switch    (long "verbose" <> short 'v' <> help "Show detailed error messages")
+  <*> strOption (long "config"  <> short 'c' <> help "The path to the config file" <> value "./config")
 
 app :: Config -> Application
 app c req respond = uncurry runPage $ route c req
@@ -33,8 +35,8 @@ app c req respond = uncurry runPage $ route c req
 
 main :: IO ()
 main = do
-  verbose <- execParser $ info (helper <*> cli) (fullDesc <> progDesc "Start the MyQA Webserver")
-  conf <- loadConfig verbose
+  (verbose, path) <- execParser $ info (helper <*> cli) (fullDesc <> progDesc "Start the MyQA Webserver")
+  conf <- loadConfig verbose (toS path)
   putText "Creating database tables..."
   createDatabase conf
   putText "Starting webserver..."
