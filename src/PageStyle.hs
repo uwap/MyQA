b07a7fcf233c3db5006d7ebf61840093a65ea05f
uwap <me@uwap.name>
2017-01-14 14:52:43 +0100

    Add style.html

---
+++ src/PageStyle.hs
@@ -0,0 +1,21 @@
+{-# LANGUAGE OverloadedStrings #-}
+module PageStyle where
+
+import Clay
+
+style :: Css
+style = body ? do
+  margin (px 0) (px 0) (px 0) (px 0)
+  nav ? do
+    backgroundColor (Rgba 200 200 200 0.5)
+    borderBottom solid (px 1) black
+    padding (px 5) (px 5) (px 5) (px 5)
+    overflow hidden
+    ul ? do
+      margin (px 0) (px 0) (px 0) (px 0)
+      listStyleType none
+      li ? do
+        float floatLeft
+        border dotted (px 1) black
+  div # ".content" ? do
+    padding (px 10) (px 10) (px 10) (px 10)
--- src/Pages.hs
+++ src/Pages.hs
@@ -3,6 +3,7 @@ module Pages where
 
 import Protolude
 import Lucid
+import Clay (render)
 import Network.HTTP.Types
 import Network.Wai
 import Network.Wai.Parse
@@ -10,10 +11,13 @@ import Data.List (lookup)
 
 import Types
 import Database
+import PageStyle
 
 page_ :: Page -> Page
 page_ p = doctypehtml_ $ do
-            head_ $ meta_ [ charset_ "utf-8" ]
+            head_ $ do
+              meta_ [ charset_ "utf-8" ]
+              link_ [ rel_ "stylesheet", type_ "text/css", href_ "/style.css" ]
             body_ $ do
               nav_ $
                 ul_ $ do
@@ -25,13 +29,13 @@ index :: Page
 index = page_ "hello world" 
 
 userGet :: Config -> Text -> Page
-userGet c t = user' c t $ 
+userGet c t = user_ c t $ 
             with form_ [ method_ "POST" ] $ do
               with textarea_ [ name_ "question" ] ""
               input_ [ type_ "submit", value_ "Ask!" ]
 
 userPost :: Request -> Config -> Text -> Page
-userPost r c t = user' c t $ do
+userPost r c t = user_ c t $ do
             rt <- liftIO $ parseRequestBody lbsBackEnd r
             let question = lookup "question" $ fst rt
             case question of
@@ -40,14 +44,12 @@ userPost r c t = user' c t $ do
                 p_ "The question was added successfully"
               Nothing -> p_ "Something went wrong"
 
-user' :: Config -> Text -> Page -> Page
-user' c t p = do
+user_ :: Config -> Text -> Page -> Page
+user_ c t p = do
   r <- questionsWithReply c t
   case r of
     [] -> page404
-    _  -> page_ $ do
-            p
-            forM_ r showQuestion
+    _  -> page_ $ p >> with div_ [ id_ "questions" ] (forM_ r showQuestion)
 
 showQuestion :: (Text,Text) -> Page
 showQuestion (t, p) = with div_ [ class_ "question" ] $ do
@@ -66,10 +68,11 @@ route c r | requestMethod r == methodGet = routeGet
           | otherwise = (status405, page405)
   where
     routeGet = case pathInfo r of
-      []         -> (status200, index)
-      ["user",i] -> (status200, userGet c i)
-      ["418"]    -> (status418, page_ "I'm a teapot")
-      _          -> (status404, page404)
+      []            -> (status200, index)
+      ["user",i]    -> (status200, userGet c i)
+      ["418"]       -> (status418, page_ "I'm a teapot")
+      ["style.css"] -> (status200, toHtmlRaw (render style))
+      _             -> (status404, page404)
     routePost = case pathInfo r of
       ["user",i] -> (status200, userPost r c i)
       _          -> (status404, page404)
